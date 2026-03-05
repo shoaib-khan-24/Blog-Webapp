b352554c9d2b3128b47abb3719c62949b92e7e9d
ShoaiB_KhaN <143168713+shoaib-khan-24@users.noreply.github.com>
2026-03-05 11:56:56 +0530

    Update main.py
    Add twilio SMS service and minor changes.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime
 from flask import Flask, abort, render_template, redirect, url_for, flash
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
@@ -8,8 +8,12 @@ from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text, ForeignKey
 from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
-from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm, ContactForm
 from flask_gravatar import Gravatar
+from twilio.rest import Client
+from dotenv import load_dotenv
+import os
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -42,6 +46,15 @@ gravatar = Gravatar(
     use_ssl=False,
     base_url=None
 )
+
+
+#twilio credentials
+load_dotenv()
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+twilio_phone = os.getenv("TWILIO_PHONE")
+admin_phone = os.getenv("ADMIN_PHONE")
+
 
 # CONFIGURE TABLES
 class User(UserMixin, db.Model):
@@ -127,8 +140,8 @@ def register():
         print("User added")
         login_user(new_user)
         return redirect(url_for("get_all_posts"))
-
-    return render_template("register.html", form=form)
+    curr_year = datetime.today().year
+    return render_template("register.html", form=form, curr_year=curr_year)
 
 
 @app.route('/login', methods=['GET','POST'])
@@ -149,8 +162,8 @@ def login():
                 flash("Wrong password! Please try again.")
         else:
             flash("Wrong email! Please try again.")
-
-    return render_template("login.html", form=form)
+    curr_year = datetime.today().year
+    return render_template("login.html", form=form, curr_year=curr_year)
 
 
 @app.route('/logout')
@@ -163,10 +176,11 @@ def logout():
 def get_all_posts():
     result = db.session.execute(db.select(BlogPost))
     posts = result.scalars().all()
-    return render_template("index.html", all_posts=posts, current_user=current_user)
+    curr_year = datetime.today().year
+    return render_template("index.html", all_posts=posts, current_user=current_user, curr_year=curr_year)
 
 
-# TODO: Allow logged-in users to comment on posts
+
 @app.route("/post/<int:post_id>", methods=['GET','POST'])
 def show_post(post_id):
     requested_post = db.get_or_404(BlogPost, post_id)
@@ -184,8 +198,8 @@ def show_post(post_id):
         db.session.add(new_comment)
         db.session.commit()
         return redirect(url_for("show_post", post_id=post_id))
-
-    return render_template("post.html", post=requested_post, form=form, current_user=current_user)
+    curr_year = datetime.today().year
+    return render_template("post.html", post=requested_post, form=form, current_user=current_user, curr_year=curr_year)
 
 
 @app.route("/new-post", methods=["GET", "POST"])
@@ -204,7 +218,8 @@ def add_new_post():
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form)
+    curr_year = datetime.today().year
+    return render_template("make-post.html", form=form, curr_year=curr_year)
 
 
 @app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
@@ -226,7 +241,8 @@ def edit_post(post_id):
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
-    return render_template("make-post.html", form=edit_form, is_edit=True)
+    curr_year = datetime.today().year
+    return render_template("make-post.html", form=edit_form, is_edit=True, curr_year=curr_year)
 
 
 @app.route("/delete/<int:post_id>")
@@ -240,12 +256,34 @@ def delete_post(post_id):
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    curr_year = datetime.today().year
+    return render_template("about.html", curr_year=curr_year)
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET','POST'])
 def contact():
-    return render_template("contact.html")
+    form = ContactForm()
+    if form.validate_on_submit():
+        #using twilio to send message to owner
+        user_name = form.name.data
+        user_email = form.email.data
+        user_phone = form.phone.data
+        user_message = form.message.data
+
+        user_complete_message = f"User {user_name} ({user_phone},{user_email}) has contacted you!\n{user_message}"
+
+        client = Client(account_sid, auth_token)
+        message = client.messages.create(
+            body = user_complete_message,
+            from_ = twilio_phone,
+            to = admin_phone,
+        )
+        print(message.body)
+
+        return redirect(url_for("get_all_posts"))
+    curr_year = datetime.today().year
+    return render_template("contact.html", form=form, curr_year=curr_year)
+
 
 
 if __name__ == "__main__":
